e28591788e6b511869afb2e85c12686a59fe4c53
Yaroslav Popov <y.popov@globexit.ru>
2025-08-24 22:32:00 +0300

    feat(wepback.config): add bundler modes and CopyHtml plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
     entry: "./index.js",
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "build")
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "build"),
+        clean: true
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public/index.html")
+    })],
 };

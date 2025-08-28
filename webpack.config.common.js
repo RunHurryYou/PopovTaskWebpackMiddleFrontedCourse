92828d5731620368ae9ea4bc979fc732626262e5
Yaroslav Popov <y.popov@globexit.ru>
2025-08-28 13:29:55 +0300

    feat(webpack.config): configurate webpack for ts

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -6,7 +6,7 @@ const { default: postcss } = require('postcss');
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
-    entry: "./index.js",
+    entry: "./index.ts",
     output: {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "build"),
@@ -51,6 +51,11 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
             },
         ],
     },
